204606d243a9def5ed41c53e525c1fd397dd751c
Pénélope <penelope.delabriere@gmail.com>
2024-02-29 11:46:04 +0100

    Resolves #18 : Create Comment view

---
+++ app/src/main/java/com/ut3/restop/LaisserAvisView.java
@@ -0,0 +1,14 @@
+package com.ut3.restop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LaisserAvisView extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.comment);
+    }
+}
--- app/src/main/java/com/ut3/restop/RestaurantView.java
+++ app/src/main/java/com/ut3/restop/RestaurantView.java
@@ -5,7 +5,11 @@ import static android.content.ContentValues.TAG;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
@@ -33,6 +37,13 @@ public class RestaurantView extends AppCompatActivity{
 
             menuContainer.addView(cardView);
         }
+
+        Button laisserAvisButton = findViewById(R.id.restau_avis);
+        laisserAvisButton.setOnClickListener(v -> {
+            Context context = v.getContext();
+            Intent intent = new Intent(context, LaisserAvisView.class);
+            context.startActivity(intent);
+        });
 
     }
 
